c249df91189343a8cd60d7c7d6ba3e28dc442da9
Atharva Joshi <iamatharvaj@gmail.com>
2021-08-04 10:57:45 +0530

    Added javascript hoisting

--- hoisting.js
+++ hoisting.js
@@ -1,2 +1,10 @@
 // HOISTING
 // Accessing varibles and functions even before initializing it, or putting some value in it, without any error
+getValue();
+console.log("value of a :: ", a);
+console.log(getValue);
+var a = 5; // try with let and const
+// allocate space for this function at the beginning of execution
+function getValue() {
+    console.log("function getValue() just called!");
+}
