106709f6a24d1d7d9218a121653f748ea507a9f4
Atharva Joshi <iamatharvaj@gmail.com>
2021-08-14 10:57:45 +0530

    Added javascript call-apply-bind

--- callApplyBind.js
+++ callApplyBind.js
@@ -9,3 +9,11 @@ let printDetails = function (year, color) {
 // call method used to invoke a function directly by passsing the reference (car) which points to this variable inside the method (printDetails)
 printDetails.call(car, "2021", "White");
 
+let bike = {
+    company: "Royal Enfield",
+    model: "Interceptor 650"
+}
+
+// apply is eactly same as call method, difference is that it takes second argument as array list of the parameters which need to be passed to the printDetails function
+printDetails.apply(bike, ["2020", "Black"]);
+
