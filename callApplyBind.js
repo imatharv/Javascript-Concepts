a70bd6d7aaa27bedf04ee4d30e4096b61004ba38
Atharva Joshi <iamatharvaj@gmail.com>
2021-08-14 10:57:45 +0530

    Added javascript call-apply-bind

--- callApplyBind.js
+++ callApplyBind.js
@@ -1,1 +1,11 @@
 // CALL, APPLY AND BIND
+let car = {
+    company: "Jeep",
+    model: "Compass"
+}
+let printDetails = function (year, color) {
+    console.log(this.company + " " + this.model + " " + year + " " + color);
+}
+// call method used to invoke a function directly by passsing the reference (car) which points to this variable inside the method (printDetails)
+printDetails.call(car, "2021", "White");
+
